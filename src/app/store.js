044ee28f9860824ff1e3b3291f2c140761157d04
prakhar2407 <prakhar19csu215@ncuindia.edu>
2022-11-25 01:48:03 +0530

    Add code for react-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../hooks/authSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 export default configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: persistedReducer,
   devTools: true,
 });
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
+let persistor = persistStore(store);
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
